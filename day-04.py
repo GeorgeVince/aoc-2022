806a508ddaf6957c5fa5186bef082feb1d056038
George <george.vince@hotmail.co.uk>
2022-12-06 13:07:16 +0000

    added: day 4

--- day-04.py
+++ day-04.py
@@ -19,3 +19,31 @@ for row in contents:
         total +=1
         continue
 print(total)
+
+total = 0
+for row in contents:
+    print(row, total)
+    elf_1, elf_2 = row.split(",")
+    e_1_lower, e_1_higher = elf_1.split("-")
+    e_2_lower, e_2_higher = elf_2.split("-")
+
+    e_1_lower = int(e_1_lower)
+    e_1_higher = int(e_1_higher)
+    e_2_lower = int(e_2_lower)
+    e_2_higher = int(e_2_higher)
+
+    if e_1_lower <= e_2_lower:
+        while e_1_lower <= e_1_higher:
+            if e_1_lower >= e_2_lower and e_1_lower <= e_2_higher:
+                total += 1
+            e_1_lower += 1
+        continue
+    
+    if e_2_lower <= e_1_lower:
+        while e_2_lower <= e_2_higher:
+            if e_2_lower >= e_1_lower and e_1_lower <= e_1_higher:
+                total += 1
+            e_2_lower += 1
+        continue
+    
+print(total)
