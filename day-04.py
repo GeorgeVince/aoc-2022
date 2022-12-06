d929b10b8596490e4b3d2406965709af02bf1cf3
George <george.vince@hotmail.co.uk>
2022-12-06 12:50:30 +0000

    added: day 4

---
+++ day-04.py
@@ -0,0 +1,21 @@
+with open("./input/day-04.txt", 'r') as f:
+    contents = [x.strip() for x in f.readlines()]
+
+total = 0
+for row in contents:
+    elf_1, elf_2 = row.split(",")
+    e_1_lower, e_1_higher = elf_1.split("-")
+    e_2_lower, e_2_higher = elf_2.split("-")
+
+    e_1_lower = int(e_1_lower)
+    e_1_higher = int(e_1_higher)
+    e_2_lower = int(e_2_lower)
+    e_2_higher = int(e_2_higher)
+
+    if e_1_lower >= e_2_lower and e_1_higher <= e_2_higher:
+        total +=1
+        continue
+    if e_1_lower <= e_2_lower and e_1_higher >= e_2_higher:
+        total +=1
+        continue
+print(total)
